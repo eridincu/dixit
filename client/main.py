8dfbe974d6ddc120d3cb4e50b39ef562c343f218
firatbulut19 <44057526+firatbulut19@users.noreply.github.com>
2021-01-15 23:27:21 +0300

    groundwork done.

---
+++ client/main.py
@@ -0,0 +1,79 @@
+# SEND TCP:
+
+# 1. selected image
+# 2. storyteller sends description
+# 3. non-storytellers vote image
+# 4. goodbye
+
+
+def listen_tcp():
+    while(True):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((HOST, PORT))
+            s.settimeout(5)
+            try:
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        stri = data.decode('utf-8').rstrip()
+                        dic = eval(stri)
+                        if dic["TYPE"] == "DECK_IMG":
+                            # TO DO
+                            pass
+                        elif dic["TYPE"] == "DECK_INIT":
+							# TO DO
+							pass
+                        else:
+                            # TO dO
+                            pass
+            except socket.timeout:
+                pass
+
+            s.close()
+
+
+
+def listen_udp():
+    while(True):
+        #print("in broadcast")
+        port_ = 12345
+        bufferSize = 1024
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.settimeout(5)
+        try:
+            s.bind(('', port_))
+            #print("in broadcast")
+            s.setblocking(0)
+
+            result = select.select([s], [], [])
+            msg = result[0][0].recv(bufferSize)
+            stri = msg.decode('utf-8').rstrip()
+            dic = eval(stri)
+            if dic["TYPE"] == "DISCOVER":
+                # TO DO
+                pass
+            elif dic["TYPE"] == "GOODBYE":
+                # TO DO
+                pass
+            elif dic["TYPE"] == "STORYTELLER":
+                # TO DO
+                pass
+            elif dic["TYPE"] == "POINTS":
+                # TO DO
+                pass
+            elif dic["TYPE"] == "POOL_IMAGES":
+				# TO DO
+				pass
+			elif dic["TYPE"] == "DESCRIPTION":
+				# TO DO
+				pass
+            #print("in broadcast")
+        except socket.timeout:
+            pass
+        s.close()
---
+++ server/main.py
@@ -0,0 +1,26 @@
+# Listen TCP:
+
+# 1. receive selected image from everyone
+# 2. storyteller sends description
+# 3. non-storytellers vote image
+# 4. receive goodbye
+
+# Send TCP:
+
+# 1. send image X6 to everyone.
+# 2. send 1 image at the begining of each round to everyone.
+
+# Send UDP:
+
+# 1. send online users.
+# 2. send left users.
+# 3. send who storyteller is
+# 4. send description
+# 5. send pool images
+# 6. send end of round info:
+#   a. points
+#   b. who picked which image
+#   c. storyteller's image
+#   d. next storyteller?
+# 7. start of new round message
+
